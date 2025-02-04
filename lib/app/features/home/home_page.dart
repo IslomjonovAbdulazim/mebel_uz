42effe8398f75701a73017a680a16311b0b57290
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-04 22:11:58 +0500

    imp

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -1,1 +1,11 @@
 part of 'imports.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
