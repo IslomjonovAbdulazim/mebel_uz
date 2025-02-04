320be80f4984b90dcafabf7a6ea6eb97f1c1e501
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-04 22:43:05 +0500

    imp

--- lib/utils/constants/color_constants.dart
+++ lib/utils/constants/color_constants.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-class ColorConstants {
-  ColorConstants._();
-
-  static const Color white = Color(0xffffffff);
-  static const Color black = Color(0xff000000);
-  static const Color lightBlue = Color(0xffD6DFFF);
-  static const Color lighterBlue = Color(0xffF5F9FE);
-  static const Color borderGrey = Color(0xffEAEFF5);
-  static const Color blue = Color(0xff3461fd);
-  static const Color darkBlue = Color(0xff2b4eca);
-  static const Color grey = Color(0xff8491a6);
-  static const Color darkGrey = Color(0xff3e4256);
-
-  static const Color dark = Color(0xff252F40);
-  static const Color light = Color(0xff838DA0);
-}
-
 class DarkColorConstants {
   static const Color primary = Color(0xFFEAEAEA);
   static const Color background = Color(0xFF121212);
