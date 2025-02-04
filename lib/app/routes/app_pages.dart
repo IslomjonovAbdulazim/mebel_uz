6cf4bc27c3377f7a969b36116a46a3b953b5a57e
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-04 22:20:17 +0500

    imp

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:wordly_project/app/bindings/contacts_binding.dart';
 import 'package:wordly_project/app/bindings/home_binding.dart';
 import 'package:wordly_project/app/features/contacts/imports.dart';
+import 'package:wordly_project/app/features/detail/imports.dart';
 import 'package:wordly_project/app/features/home/imports.dart';
 import 'package:wordly_project/app/routes/app_routes.dart';
 
@@ -16,6 +18,13 @@ class AppPages {
         GetPage(
           name: AppRoutes.contacts,
           page: () => ContactsPage(),
+          bindings: [
+            ContactsBinding(),
+          ],
+        ),
+        GetPage(
+          name: AppRoutes.detail,
+          page: () => DetailPage(),
         ),
       ],
     ),
