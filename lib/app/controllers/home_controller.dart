56d84fb250c4beb30c7c6d182ebfc5b4126a7b8d
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-06 14:08:01 +0500

    imp

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,18 +1,18 @@
 import 'package:get/get.dart';
-import 'package:wordly_project/app/data/models/hospital_furniture_model.dart';
-import 'package:wordly_project/utils/helpers/status_code_helper.dart';
 
+import '../../domain/entities/furniture_entity.dart';
 import '../../domain/respositories/home_repository.dart';
+import '../../main.dart';
+import '../../utils/helpers/status_code_helper.dart';
 
 class HomeController extends GetxController {
   RxBool isLoading = false.obs;
-  Rx<List<HospitalFurnitureModel>> furniture =
-      Rx<List<HospitalFurnitureModel>>([]);
+  Rx<List<FurnitureEntity>> furniture = Rx<List<FurnitureEntity>>([]);
 
   Future<void> init() async {
     isLoading.value = true;
     final api = Get.find<HomeRepository>();
-    final result = await api.furniture();
+    final result = await api.allFurniture();
     await Future.delayed(Duration(seconds: 3));
     isLoading.value = false;
 
@@ -29,6 +29,13 @@ class HomeController extends GetxController {
       },
     );
 
-    furniture.value = [];
+    furniture.value = List.generate(10, (int index) {
+      return FurnitureEntity(
+        id: "-1",
+        image: "",
+        model: faker.lorem.word(),
+        price: faker.currency.random.integer(10000000, min: 100000),
+      );
+    });
   }
 }
