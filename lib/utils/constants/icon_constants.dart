fb3cdb19304e47126641d16aac7c8a639ba063e1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-06 14:45:56 +0500

    imp

--- lib/utils/constants/icon_constants.dart
+++ lib/utils/constants/icon_constants.dart
@@ -1,12 +1,7 @@
 class IconConstants {
   IconConstants._();
 
-  static const String back = 'assets/icons/back.png';
-  static const String check = 'assets/icons/check.png';
-  static const String crown = 'assets/icons/crown.svg';
-  static const String facebook = 'assets/icons/facebook.png';
-  static const String google = 'assets/icons/google.png';
-  static const String leaderboard = 'assets/icons/leaderboard.svg';
-  static const String noChange = 'assets/icons/no_change.png';
-  static const String sound = 'assets/icons/sound.png';
+  static const String arrowLeft = 'assets/icons/arrow-left.svg';
+  static const String arrowRight = 'assets/icons/arrow-right.svg';
+  static const String connectionLost = 'assets/icons/connection-lost.svg';
 }
--- lib/utils/constants/image_constants.dart
+++ lib/utils/constants/image_constants.dart
@@ -5,7 +5,7 @@ class ImageConstants {
       "assets/images/welcome_background.png";
   static const String welcomeImage = "assets/images/welcome_image.png";
 
-  static const String connectionLost = 'assets/icons/connection_lost.svg';
+  static const String connectionLost = 'assets/icons/connection-lost.svg';
   static const String notFound = 'assets/icons/not_found.svg';
   static const String error404 = 'assets/icons/404.svg';
 }
