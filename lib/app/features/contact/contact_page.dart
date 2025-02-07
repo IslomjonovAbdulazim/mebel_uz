d1a425ddf566addc65e91f57dcf6d5f5470f2590
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 12:10:54 +0500

    imp

--- lib/app/features/contact/contact_page.dart
+++ lib/app/features/contact/contact_page.dart
@@ -5,6 +5,8 @@ class ContactsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
