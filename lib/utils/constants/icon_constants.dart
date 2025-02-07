7215bdbda3afa8966359f1a4604903aec8792280
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 12:24:01 +0500

    imp

--- lib/utils/constants/icon_constants.dart
+++ lib/utils/constants/icon_constants.dart
@@ -5,4 +5,9 @@ class IconConstants {
   static const String phone = 'assets/icons/phone.png';
   static const String arrowRight = 'assets/icons/arrow-right.svg';
   static const String connectionLost = 'assets/icons/connection-lost.svg';
+  static const String telegram = 'assets/icons/telegram.png';
+  static const String instagram = 'assets/icons/instagram.png';
+  static const String website = 'assets/icons/website.png';
+  static const String tgBot = 'assets/icons/tg-bot.png';
+  static const String location = 'assets/icons/location.png';
 }
