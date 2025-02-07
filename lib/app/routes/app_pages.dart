80e5e8c4bf1eb522f2b011827b9399b49ac56f51
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 10:53:43 +0500

    imp

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,13 +1,15 @@
 import 'package:get/get.dart';
-import 'package:wordly_project/app/bindings/company_binding.dart';
-import 'package:wordly_project/app/bindings/home_repository_iml_binding.dart';
-import 'package:wordly_project/app/features/company/imports.dart';
 
+import '../bindings/company_binding.dart';
 import '../bindings/contact_binding.dart';
 import '../bindings/detail_binding.dart';
 import '../bindings/home_binding.dart';
+import '../bindings/home_repository_iml_binding.dart';
+import '../features/category/imports.dart';
+import '../features/company/imports.dart';
 import '../features/contact/imports.dart';
 import '../features/detail/imports.dart';
+import '../features/discount/imports.dart';
 import '../features/home/imports.dart';
 import '../features/screens/offline/imports.dart';
 import 'app_routes.dart';
@@ -25,14 +27,12 @@ class AppPages {
         HomeBinding(),
         HomeRepositoryImlBinding(),
       ],
-      children: [
-        GetPage(
-          name: AppRoutes.company,
-          page: () => CompanyPage(),
-          bindings: [
-            CompanyBinding(),
-          ],
-        ),
+    ),
+    GetPage(
+      name: AppRoutes.company,
+      page: () => CompanyPage(),
+      bindings: [
+        CompanyBinding(),
       ],
     ),
     GetPage(
@@ -41,14 +41,26 @@ class AppPages {
       bindings: [
         DetailBinding(),
       ],
-      children: [
-        GetPage(
-          name: AppRoutes.contact,
-          page: () => ContactsPage(),
-          bindings: [
-            ContactBinding(),
-          ],
-        ),
+    ),
+    GetPage(
+      name: AppRoutes.contact,
+      page: () => ContactsPage(),
+      bindings: [
+        ContactBinding(),
+      ],
+    ),
+    GetPage(
+      name: AppRoutes.discount,
+      page: () => DiscountPage(),
+      bindings: [
+        ContactBinding(),
+      ],
+    ),
+    GetPage(
+      name: AppRoutes.category,
+      page: () => CategoryPage(),
+      bindings: [
+        ContactBinding(),
       ],
     ),
   ];
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,6 +1,8 @@
 class AppRoutes {
   static const String home = "/home";
   static const String detail = "$home/detail";
+  static const String discount = "$home/discount";
+  static const String category = "$home/category";
   static const String company = "$home/company";
   static const String contact = "$detail/contact";
   static const String offline = "/offline";
