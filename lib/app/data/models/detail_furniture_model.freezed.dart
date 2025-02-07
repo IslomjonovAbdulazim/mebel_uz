80b5788f67de999149227c1d56655d2698c4ad65
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 20:05:43 +0500

    imp

---
+++ lib/app/data/models/detail_furniture_model.freezed.dart
@@ -0,0 +1,424 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'detail_furniture_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+DetailFurnitureModel _$DetailFurnitureModelFromJson(Map<String, dynamic> json) {
+  return _DetailFurnitureModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$DetailFurnitureModel {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  int get price => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  double get weight => throw _privateConstructorUsedError;
+  int get height => throw _privateConstructorUsedError;
+  int get width => throw _privateConstructorUsedError;
+  bool get discount => throw _privateConstructorUsedError;
+  @JsonKey(name: 'discount_start')
+  String get discountStart => throw _privateConstructorUsedError;
+  @JsonKey(name: 'discount_end')
+  String get discountEnd => throw _privateConstructorUsedError;
+  @JsonKey(name: 'discount_percent')
+  int get discountPercentage => throw _privateConstructorUsedError;
+  List<String> get images => throw _privateConstructorUsedError;
+
+  /// Serializes this DetailFurnitureModel to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of DetailFurnitureModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $DetailFurnitureModelCopyWith<DetailFurnitureModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DetailFurnitureModelCopyWith<$Res> {
+  factory $DetailFurnitureModelCopyWith(DetailFurnitureModel value,
+          $Res Function(DetailFurnitureModel) then) =
+      _$DetailFurnitureModelCopyWithImpl<$Res, DetailFurnitureModel>;
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      int price,
+      String description,
+      double weight,
+      int height,
+      int width,
+      bool discount,
+      @JsonKey(name: 'discount_start') String discountStart,
+      @JsonKey(name: 'discount_end') String discountEnd,
+      @JsonKey(name: 'discount_percent') int discountPercentage,
+      List<String> images});
+}
+
+/// @nodoc
+class _$DetailFurnitureModelCopyWithImpl<$Res,
+        $Val extends DetailFurnitureModel>
+    implements $DetailFurnitureModelCopyWith<$Res> {
+  _$DetailFurnitureModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of DetailFurnitureModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? price = null,
+    Object? description = null,
+    Object? weight = null,
+    Object? height = null,
+    Object? width = null,
+    Object? discount = null,
+    Object? discountStart = null,
+    Object? discountEnd = null,
+    Object? discountPercentage = null,
+    Object? images = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      weight: null == weight
+          ? _value.weight
+          : weight // ignore: cast_nullable_to_non_nullable
+              as double,
+      height: null == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as int,
+      width: null == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as int,
+      discount: null == discount
+          ? _value.discount
+          : discount // ignore: cast_nullable_to_non_nullable
+              as bool,
+      discountStart: null == discountStart
+          ? _value.discountStart
+          : discountStart // ignore: cast_nullable_to_non_nullable
+              as String,
+      discountEnd: null == discountEnd
+          ? _value.discountEnd
+          : discountEnd // ignore: cast_nullable_to_non_nullable
+              as String,
+      discountPercentage: null == discountPercentage
+          ? _value.discountPercentage
+          : discountPercentage // ignore: cast_nullable_to_non_nullable
+              as int,
+      images: null == images
+          ? _value.images
+          : images // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$DetailFurnitureModelImplCopyWith<$Res>
+    implements $DetailFurnitureModelCopyWith<$Res> {
+  factory _$$DetailFurnitureModelImplCopyWith(_$DetailFurnitureModelImpl value,
+          $Res Function(_$DetailFurnitureModelImpl) then) =
+      __$$DetailFurnitureModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      int price,
+      String description,
+      double weight,
+      int height,
+      int width,
+      bool discount,
+      @JsonKey(name: 'discount_start') String discountStart,
+      @JsonKey(name: 'discount_end') String discountEnd,
+      @JsonKey(name: 'discount_percent') int discountPercentage,
+      List<String> images});
+}
+
+/// @nodoc
+class __$$DetailFurnitureModelImplCopyWithImpl<$Res>
+    extends _$DetailFurnitureModelCopyWithImpl<$Res, _$DetailFurnitureModelImpl>
+    implements _$$DetailFurnitureModelImplCopyWith<$Res> {
+  __$$DetailFurnitureModelImplCopyWithImpl(_$DetailFurnitureModelImpl _value,
+      $Res Function(_$DetailFurnitureModelImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of DetailFurnitureModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? price = null,
+    Object? description = null,
+    Object? weight = null,
+    Object? height = null,
+    Object? width = null,
+    Object? discount = null,
+    Object? discountStart = null,
+    Object? discountEnd = null,
+    Object? discountPercentage = null,
+    Object? images = null,
+  }) {
+    return _then(_$DetailFurnitureModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as int,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      weight: null == weight
+          ? _value.weight
+          : weight // ignore: cast_nullable_to_non_nullable
+              as double,
+      height: null == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as int,
+      width: null == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as int,
+      discount: null == discount
+          ? _value.discount
+          : discount // ignore: cast_nullable_to_non_nullable
+              as bool,
+      discountStart: null == discountStart
+          ? _value.discountStart
+          : discountStart // ignore: cast_nullable_to_non_nullable
+              as String,
+      discountEnd: null == discountEnd
+          ? _value.discountEnd
+          : discountEnd // ignore: cast_nullable_to_non_nullable
+              as String,
+      discountPercentage: null == discountPercentage
+          ? _value.discountPercentage
+          : discountPercentage // ignore: cast_nullable_to_non_nullable
+              as int,
+      images: null == images
+          ? _value._images
+          : images // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$DetailFurnitureModelImpl implements _DetailFurnitureModel {
+  const _$DetailFurnitureModelImpl(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.description,
+      required this.weight,
+      required this.height,
+      required this.width,
+      required this.discount,
+      @JsonKey(name: 'discount_start') required this.discountStart,
+      @JsonKey(name: 'discount_end') required this.discountEnd,
+      @JsonKey(name: 'discount_percent') required this.discountPercentage,
+      required final List<String> images})
+      : _images = images;
+
+  factory _$DetailFurnitureModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$DetailFurnitureModelImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final String name;
+  @override
+  final int price;
+  @override
+  final String description;
+  @override
+  final double weight;
+  @override
+  final int height;
+  @override
+  final int width;
+  @override
+  final bool discount;
+  @override
+  @JsonKey(name: 'discount_start')
+  final String discountStart;
+  @override
+  @JsonKey(name: 'discount_end')
+  final String discountEnd;
+  @override
+  @JsonKey(name: 'discount_percent')
+  final int discountPercentage;
+  final List<String> _images;
+  @override
+  List<String> get images {
+    if (_images is EqualUnmodifiableListView) return _images;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_images);
+  }
+
+  @override
+  String toString() {
+    return 'DetailFurnitureModel(id: $id, name: $name, price: $price, description: $description, weight: $weight, height: $height, width: $width, discount: $discount, discountStart: $discountStart, discountEnd: $discountEnd, discountPercentage: $discountPercentage, images: $images)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DetailFurnitureModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.price, price) || other.price == price) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.weight, weight) || other.weight == weight) &&
+            (identical(other.height, height) || other.height == height) &&
+            (identical(other.width, width) || other.width == width) &&
+            (identical(other.discount, discount) ||
+                other.discount == discount) &&
+            (identical(other.discountStart, discountStart) ||
+                other.discountStart == discountStart) &&
+            (identical(other.discountEnd, discountEnd) ||
+                other.discountEnd == discountEnd) &&
+            (identical(other.discountPercentage, discountPercentage) ||
+                other.discountPercentage == discountPercentage) &&
+            const DeepCollectionEquality().equals(other._images, _images));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      name,
+      price,
+      description,
+      weight,
+      height,
+      width,
+      discount,
+      discountStart,
+      discountEnd,
+      discountPercentage,
+      const DeepCollectionEquality().hash(_images));
+
+  /// Create a copy of DetailFurnitureModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$DetailFurnitureModelImplCopyWith<_$DetailFurnitureModelImpl>
+      get copyWith =>
+          __$$DetailFurnitureModelImplCopyWithImpl<_$DetailFurnitureModelImpl>(
+              this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$DetailFurnitureModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _DetailFurnitureModel implements DetailFurnitureModel {
+  const factory _DetailFurnitureModel(
+      {required final int id,
+      required final String name,
+      required final int price,
+      required final String description,
+      required final double weight,
+      required final int height,
+      required final int width,
+      required final bool discount,
+      @JsonKey(name: 'discount_start') required final String discountStart,
+      @JsonKey(name: 'discount_end') required final String discountEnd,
+      @JsonKey(name: 'discount_percent') required final int discountPercentage,
+      required final List<String> images}) = _$DetailFurnitureModelImpl;
+
+  factory _DetailFurnitureModel.fromJson(Map<String, dynamic> json) =
+      _$DetailFurnitureModelImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  String get name;
+  @override
+  int get price;
+  @override
+  String get description;
+  @override
+  double get weight;
+  @override
+  int get height;
+  @override
+  int get width;
+  @override
+  bool get discount;
+  @override
+  @JsonKey(name: 'discount_start')
+  String get discountStart;
+  @override
+  @JsonKey(name: 'discount_end')
+  String get discountEnd;
+  @override
+  @JsonKey(name: 'discount_percent')
+  int get discountPercentage;
+  @override
+  List<String> get images;
+
+  /// Create a copy of DetailFurnitureModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$DetailFurnitureModelImplCopyWith<_$DetailFurnitureModelImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
