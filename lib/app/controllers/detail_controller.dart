1b3ddfdb98a609683bd9c26ad6a8c6b0701fb887
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 10:16:48 +0500

    imp

--- lib/app/controllers/detail_controller.dart
+++ lib/app/controllers/detail_controller.dart
@@ -1,3 +1,15 @@
 import 'package:get/get.dart';
+import 'package:wordly_project/app/data/models/detail_furniture_model.dart';
+import 'package:wordly_project/utils/helpers/logger.dart';
 
-class DetailController extends GetxController {}
+class DetailController extends GetxController {
+  RxBool isLoading = false.obs;
+  Rx<DetailFurnitureModel>? furniture;
+
+  Future <void> init() async {
+    Logger.log("ID: ${Get.arguments}");
+    isLoading.value = true;
+    await Future.delayed(Duration(seconds: 1));
+    isLoading.value = false;
+  }
+}
