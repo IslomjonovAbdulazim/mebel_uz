6c1696c6cef2e25dc5d9ab2460e0ffe5a426fd5d
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 15:33:08 +0500

    imp

--- lib/app/controllers/contact_controller.dart
+++ lib/app/controllers/contact_controller.dart
@@ -1,3 +1,31 @@
 import 'package:get/get.dart';
+import 'package:wordly_project/domain/entities/company_entity.dart';
 
-class ContactController extends GetxController {}
+class ContactController extends GetxController {
+  Rx<CompanyEntity>? company;
+  RxBool isLoading = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    init();
+  }
+
+  Future<void> init() async {
+    isLoading.value = false;
+    await Future.delayed(Duration(seconds: 1));
+    // company!.value = CompanyEntity(
+    //   instagramChannel: instagramChannel,
+    //   telegramChannel: telegramChannel,
+    //   website: website,
+    //   telegramBot: telegramBot,
+    //   phoneNumbers: phoneNumbers,
+    //   adminName: adminName,
+    //   adminUrl: adminUrl,
+    //   lat: lat,
+    //   lan: lan,
+    //   images: images,
+    // );
+    isLoading.value = true;
+  }
+}
