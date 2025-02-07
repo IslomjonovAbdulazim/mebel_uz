4fe75dedbdabe7d856954360c7ad7d46c654caae
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-07 11:27:51 +0500

    imp

---
+++ lib/utils/constants/brand_constants.dart
@@ -0,0 +1,6 @@
+class BrandConstants {
+  static const String darkLong = "assets/brand/dark_long.png";
+  static const String darkShort = "assets/brand/dark_short.png";
+  static const String lightLong = "assets/brand/light_long.png";
+  static const String lightShort = "assets/brand/light_short.png";
+}
