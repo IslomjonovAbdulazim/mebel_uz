958f66495355eaf281857ba37f80411e62d030a6
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-11 11:07:59 +0500

    imp 475

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,8 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.find<ThemeController>();
